66ac4cfe3610543b374880c009b202d0cbf01065
Thomas <tgudin@publidom.com>
2016-03-24 16:20:11 -0400

    Prise en compte du multi map sur la page

--- front/generateur-map.js
+++ front/generateur-map.js
@@ -1,37 +1,52 @@
 
-
-/*----------------REQUETE AJAX-------------------*/
-var points = {};
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET", "../../wp-admin/admin-ajax.php?action=mon_action", false);
-xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        points = JSON.parse(xmlhttp.responseText);
-    }
-  
-};
-xmlhttp.send(null);
-
-
 /*--------------MAP-----------------*/
 
 /*Variable pour infobulle*/
 var infowindow;
 
 function initialize() {
-    /*Création de la map*/
-    var map = new google.maps.Map(document.getElementById('map_canvas'), {
-        center: new google.maps.LatLng(14.6500000, -61.0297823),
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoom: 11
-    });
 
-    /*Parcour du JSON*/
-    for (var i in points) {
-        var building = points[i];
-        var location = new google.maps.LatLng(building.lat, building.lng);
-        addMarker(map, building.name, building.description, location, building.statut, building.desc_alert);
-    }
+    /****** Récupération de toutes les maps ******/
+    jQuery(function ($) {
+
+        $('.map_canvas').each(function () {
+            var lattitude = $(this).attr('lat');
+            var longitude = $(this).attr('long');
+            Creationmaps(this.id, lattitude, longitude);
+        }
+        );
+
+
+        function Creationmaps(carte, lattitude, longitude) {
+            /*Création de la map*/
+            var map = new google.maps.Map(document.getElementById(carte), {
+                center: new google.maps.LatLng(lattitude, longitude),
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                zoom: 11
+            });
+
+
+            /*----------------REQUETE AJAX-------------------*/
+            var points = {};
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("GET", "../../wp-admin/admin-ajax.php?action=mon_action", false);
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                    points = JSON.parse(xmlhttp.responseText);
+                }
+
+            };
+            xmlhttp.send(null);
+
+
+            /*Parcour du JSON*/
+            for (var i in points) {
+                var building = points[i];
+                var location = new google.maps.LatLng(building.lat, building.lng);
+                addMarker(map, building.name, building.description, location, building.statut, building.desc_alert);
+            }
+        }
+    });
 }
 
 
@@ -48,7 +63,7 @@ function addMarker(map, name, description, location, statut, messageAlert) {
             icon = "fonctionnement.png";
             break;
     }
-    icon = "/wp-content/plugins/carte-coupures/front/assets/" + icon; 
+    icon = "/wp-content/plugins/carte-coupures/front/assets/" + icon;
 
     /*icon*/
     var marker = new google.maps.Marker({
@@ -60,19 +75,18 @@ function addMarker(map, name, description, location, statut, messageAlert) {
 
 
     /*----Infos bulle-----*/
-       
-        /*Création du contenu*/
-        var signalerButton = '';
-        if(statut == 1){/* Si "en fonctionnement" */
-           signalerButton = '<a href="/contact">Signaler une panne</a>';
-           messageAlert = '';
-        }
-        else if(messageAlert && statut == 0) { /*Si "en panne" et qu'une date à été rentrée*/
-          messageAlert = '<div class="alert" style="color:red;"><strong>' + messageAlert + '</strong></div>';  
-        }
-        
-        var contentString = '<h3>'+ name +'</h3>' +
-        '<p>'+ description +'</p>' + signalerButton + messageAlert;
+
+    /*Création du contenu*/
+    var signalerButton = '';
+    if (statut == 1) {/* Si "en fonctionnement" */
+        signalerButton = '<a href="/contact">Signaler une panne</a>';
+        messageAlert = '';
+    } else if (messageAlert && statut == 0) { /*Si "en panne" et qu'une date à été rentrée*/
+        messageAlert = '<div class="alert" style="color:red;"><strong>' + messageAlert + '</strong></div>';
+    }
+
+    var contentString = '<h3>' + name + '</h3>' +
+            '<p>' + description + '</p>' + signalerButton + messageAlert;
 
 
     /*Info bulle*/
